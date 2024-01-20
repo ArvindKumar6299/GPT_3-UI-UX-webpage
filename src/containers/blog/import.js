83db1e5930ea55f8380da344d05e3fb16962b389
ArvindKumar6299 <arvindkumar735240@gmail.com>
2024-01-21 03:40:06 +0530

    gpt3 ui/ux project completed

---
+++ src/containers/blog/import.js
@@ -0,0 +1,13 @@
+import blog01 from '../../assets/three.png';
+import blog02 from '../../assets/two.png';
+import blog03 from '../../assets/six.png';
+import blog04 from '../../assets/four.png';
+import blog05 from '../../assets/five.png';
+
+export{
+    blog01,
+    blog02,
+    blog03,
+    blog04,
+    blog05,
+}
